510137d95601cc4f37ab6c1fe78d02bc5534cba0
Mohamed Hayibor <mohamedhayibor@gmail.com>
2017-03-19 12:57:04 +0000

    Add naive parsing of donation amount

--- processIssuePayload.js
+++ processIssuePayload.js
@@ -8,31 +8,42 @@
  * > when pr gets merged check if we had that task on our db
  */
 
+const parseNum = require('parse-num');
+
 function processIssuePayload(payload) {
+  const commenterLogin = payload.comment.user.login;
+  const commentText = payload.comment.body;
+  const issueTitle = payload.issue.title;
+  const issueNumber = payload.issue.number;
+
+  console.log("commenter login: ", commenterLogin);
+  console.log("commenter text: ", commentText);
+  console.log("issue text: ", issueTitle);
+  console.log("issue Number: ", issueNumber, "\n\n");
+
   const action = payload.action;
 
   switch (action) {
     case "opened":
       // TODO
-      console.log("user opened new issue"); break;
+      // This fails in the entry of multiple numbers, ie: 40 43.5  34
+      const sponsoringAmount = Math.abs( paseNum(issueTitle) );
+
+      // cap the current donations to $70 to mitigate major screwups
+      if (sponsoringAmount <= 70) {
+        console.log(`>>> Wire ${sponsoringAmount} from ${commenterLogin}'s account`);
+      } else {
+        console.log("Waouh, that's either a high donation or we couldn't parse it. Contact support");
+      }
 
     case "created":
-      // TODO
+      // TODO: not needed for the moment
       console.log("user submitted a new comment"); break;
 
     default:
       console.log("We don't need that action yet"); break;
   }
 
-  const commenterLogin = payload.comment.user.login;
-  const commentText = payload.comment.body;
-  const issueText = payload.issue.title;
-  const issueNumber = payload.issue.number;
-
-  console.log("commenter login: ", commenterLogin);
-  console.log("commenter text: ", commentText);
-  console.log("issue text: ", issueText);
-  console.log("issue Number: ", issueNumber);
 }
 
 module.exports = processIssuePayload;
